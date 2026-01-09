16a1e908f1ab6e397c4fc2f2ed2f911dee75e0c0
EstebanForge <esteban@attitude.cl>
2026-01-09 14:53:06 -0300

    prod build

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInit745aea12e2040fc701846ab8dc3cf237
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit745aea12e2040fc701846ab8dc3cf237::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
